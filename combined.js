a22a617085c9f852b6ecb0641519a74ef38ad751
nothingX001 <danielcmediamanagement@gmail.com>
2024-10-04 21:43:05 -0400

    Update combined.js

--- combined.js
+++ combined.js
@@ -11,7 +11,7 @@ function handleColorChange() {
 document.addEventListener('DOMContentLoaded', function() {
     var root = document.getElementById('root');
     root.innerHTML = '<div class="color-selection-container">' +
-                     '<h1>COLOR SELECTION</h1>' +
+                     '<h1>COLOR SELECTOR</h1>' +
                      '<label>CLICK HERE TO SELECT A COLOR:</label>' +
                      '<input id="colorInput" type="color" value="#FFFFFF" onchange="handleColorChange()">' +
                      '<div id="colorDisplay" class="color-display">YOUR COLOR: #FFFFFF</div>' +
